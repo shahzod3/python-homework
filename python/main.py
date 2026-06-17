c049210f45b8c3fb7d21e4768c5b2e1e3b5dd7b1
shahzod3 <135121945+shahzod3@users.noreply.github.com>
2026-06-17 19:06:29 +0500

    Add files via upload

---
+++ python/main.py
@@ -0,0 +1,8 @@
+job_title = 'Data Analyst'
+job_location = 'Uzbekistan'
+job_salary = '200000000'
+
+def greet(title, location, salary):
+    return print(f"JOB: {title}\nLOCATION:  {location}\nSALARY: {salary}")
+
+greet(job_title, job_location, job_salary)
